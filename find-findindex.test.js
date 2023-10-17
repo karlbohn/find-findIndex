160ccada6305e9f677dc57ee08387471d8a688ec
Karl Bohn <karl@McBain>
2023-10-17 18:00:21 -0400

    removeUser working

--- find-findindex.test.js
+++ find-findindex.test.js
@@ -15,26 +15,27 @@ describe("#findUserByUsername", function() {
   });
 });
 
-// describe("#removeUser", function() {
-//   let users;
-//   beforeEach(function(){
-//       users = [
-//         { username: "mlewis" },
-//         { username: "akagen" },
-//         { username: "msmith" }
-//       ];
-//   })
-//   it("removes a user from an array", function() {
-//     removeUser(users, "mlewis");
-//     expect(users.length).toEqual(2)
-    
-//   });
-//   it("returns the removed user", function() {
-//     expect(removeUser(users,"mlewis")).toEqual({ username: "mlewis" });
-//   });
-//   it("returns undefined a user from an array", function() {
-//     expect(removeUser(users, "taco")).toEqual(undefined);
-//     expect(users.length).toEqual(3);
 
-//   });
-// });
+describe("#removeUser", function() {
+  let users;
+  beforeEach(function(){
+      users = [
+        { username: "mlewis" },
+        { username: "akagen" },
+        { username: "msmith" }
+      ];
+  })
+  it("removes a user from an array", function() {
+    removeUser(users, "mlewis");
+    expect(users.length).toEqual(2)
+    
+  });
+  it("returns the removed user", function() {
+    expect(removeUser(users,"mlewis")).toEqual({ username: "mlewis" });
+  });
+  it("returns undefined a user from an array", function() {
+    expect(removeUser(users, "taco")).toEqual(undefined);
+    expect(users.length).toEqual(3);
+
+  });
+});
